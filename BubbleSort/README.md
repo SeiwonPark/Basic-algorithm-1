24e5b40d259111aa767b516babd90852ffeae4db
seiwonpark <psw7347@gmail.com>
2020-11-15 00:40:11 +0900

    Updated

---
+++ BubbleSort/README.md
@@ -0,0 +1,20 @@
+# Bubble Sort   
+
+**ABSTRACT**
+
+Bubble sort a.k.a. sinking sort is kind of sorting algorithm that repeatedly compares adjacent elements and swaps them if they are in the wrong order.   
+
+## 1. Algorithm   
+
+
+## 2. Time Complexity   
+_NOTE: Suppose we want ascending ordered data_
+
+### Average
+   
+### Best Case
+When the given data are in a ascending order, it's O(n) by Big O notation  
+
+### Worst Case    
+When the given data are in a descending order, it's О(n^2) by Big O notation     
+
---
+++ BubbleSort/main.py
@@ -0,0 +1,31 @@
+# swap two elements in `list_name`
+def swap(list_name, i, j):
+    list_name[i], list_name[j] = list_name[j], list_name[i]
+
+
+# suppose we want ascending ordered list
+def bubbleSort(list_name):
+    for size in reversed(range(len(list_name))):
+        for i in range(size):
+            if list_name[i] > list_name[i + 1]:
+                swap(list_name, i, i + 1)
+    return list_name
+
+
+# examples and expected results
+example1 = [5, 3, 11, 8, 7]
+result1 = [3, 5, 7, 8, 11]
+
+example2 = [2, 9, 10, 11, 5, 3, 1]
+result2 = [1, 2, 3, 5, 9, 10, 11]
+
+example3 = [11, 9, 2, 0, 5, 6, 1, 8, 10, 3, 7, 4, 12]
+result3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+# if it returns False, it raises Assertion Error
+# if it returns True, nothing happens
+if __name__ == "__main__":
+    assert bubbleSort(example1) == result1
+    assert bubbleSort(example2) == result2
+    assert bubbleSort(example3) == result3

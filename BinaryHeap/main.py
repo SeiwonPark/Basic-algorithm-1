f00f686fbdfafd375658050765ca67731477cf48
seiwonpark <psw7347@gmail.com>
2020-11-18 03:14:33 +0900

    Updated

--- BinaryHeap/main.py
+++ BinaryHeap/main.py
@@ -4,53 +4,143 @@ def swap(list_name, i, j):
 
 
 # 1. suppose we want max heap
-#
-def maxHeap(list_name, parent_index):
-    temp = list_name[parent_index]
+def maxHeapify(list_name, parent_index):   # heapify in a max heap
+    left_index = 2 * parent_index          # set left child index
+    right_index = 2 * parent_index + 1     # set right child index
+    if left_index != origin - 1 and right_index >= origin:
+        return
 
-    while parent_index <= size // 2:
-        child_index = parent_index * 2
-        if child_index < size and list_name[child_index] < list_name[child_index + 1]:
-            child_index += 1
-        if temp > list_name[child_index]: break
-        list_name[parent_index] = list_name[child_index]
-        parent_index = child_index
-    list_name[parent_index] = temp
+    # in case there are two child nodes
+    if left_index != origin - 1 and right_index > size:
+        if list_name[left_index] > list_name[right_index] and list_name[left_index] > list_name[parent_index]:
+            swap(list_name, left_index, parent_index)
+        elif list_name[left_index] < list_name[right_index] and list_name[right_index] > list_name[parent_index]:
+            swap(list_name, right_index, parent_index)
+
+    # in case there's only left child
+    elif left_index > size:
+        if list_name[left_index] > list_name[parent_index]:
+            swap(list_name, left_index, parent_index)
+
+    maxHeapify(list_name, left_index)
+    maxHeapify(list_name, right_index)
 
 
-def heapSort(list_name):
+# 2. suppose we want min heap
+def minHeapify(list_name, parent_index):   # heapify in a min heap
+    left_index = 2 * parent_index          # set left child index
+    right_index = 2 * parent_index + 1     # set right child index
+    if left_index != origin - 1 and right_index >= origin:
+        return
+
+    # in case there are two child nodes
+    if left_index != origin - 1 and right_index > size:
+        if list_name[left_index] < list_name[right_index] and list_name[left_index] < list_name[parent_index]:
+            swap(list_name, left_index, parent_index)
+        elif list_name[left_index] > list_name[right_index] and list_name[right_index] < list_name[parent_index]:
+            swap(list_name, right_index, parent_index)
+
+    # in case there's only left child
+    elif left_index >= size:
+        if list_name[left_index] < list_name[parent_index]:
+            swap(list_name, left_index, parent_index)
+
+    minHeapify(list_name, left_index)
+    minHeapify(list_name, right_index)
+
+
+def maxHeapSort(list_name):   # move on steps to the root
+    list_name.insert(0, 0)    # suppose index starts from '1'
+
     global size
+    global origin
+    origin = len(list_name)
     size = len(list_name) - 1    # index of the last node
-    for i in range(size//2, 0):
-        maxHeap(list_name, i)
 
-    while size > 1:
-        swap(list_name, 0, size)
+    size //= 2
+    while size != 0:
+        maxHeapify(list_name, size)
         size -= 1
-        maxHeap(list_name, 0)
+
+    del list_name[0]   # delete '0' which was inserted in #53
 
     return list_name
 
-# 2. suppose we want min heap
+
+def minHeapSort(list_name):   # move on steps to the root
+    list_name.insert(0, 0)    # suppose index starts from '1'
+
+    global size
+    global origin
+    origin = len(list_name)
+    size = len(list_name) - 1    # index of the last node
+
+    size //= 2
+    while size != 0:
+        minHeapify(list_name, size)
+        size -= 1
+
+    del list_name[0]   # delete '0' which was inserted in #71
+
+    return list_name
+
 
 # examples and expected results
-example1 = [5, 3, 11, 8, 7]
-result1 = [11, 8, 5, 3, 7]
+max_example1 = [5, 3, 11, 8, 7]
+min_example1 = [5, 3, 11, 8, 7]
+max_result1 = [11, 8, 5, 3, 7]
+min_result1 = [3, 5, 11, 8, 7]
+# example1 looks like this:
+#             5
+#            / \
+#           3  11
+#          / \
+#         8  7
+#
+# max_result1 looks like this:
+#             11            # In a max heap, root node has the largest value
+#            / \
+#           8   5           # In this max heap, parent always has larger value than those of children
+#          / \
+#         3  7
+#
+# min_result1 looks like this:
+#             3             # In a min heap, root node has the smallest value
+#            / \
+#           5  11           # In this min heap, parent always has smaller value than those of children
+#          / \
+#         8  7
 
-example2 = [2, 9, 10, 11, 5, 3, 1]
-result2 = [11, 10, 2, 9, 5, 3, 1]
+max_example2 = [2, 9, 10, 11, 5, 3, 1]
+min_example2 = [2, 9, 10, 11, 5, 3, 1]
+max_result2 = [11, 9, 10, 2, 5, 3, 1]
+min_result2 = [1, 5, 2, 11, 9, 3, 10]
 
-example3 = [11, 9, 2, 0, 5, 6, 1, 8, 10, 3, 7, 4, 12]
-result3 = [12, 10, 11, 9, 7, 2, 1, 8, 0, 3, 5, 4, 6]
+max_example3 = [11, 9, 2, 0, 5, 6, 1, 8, 10, 3, 7, 4, 12]
+min_example3 = [11, 9, 2, 0, 5, 6, 1, 8, 10, 3, 7, 4, 12]
+max_result3 = [12, 10, 11, 9, 7, 6, 1, 8, 0, 3, 5, 4, 2]
+min_result3 = [0, 3, 1, 8, 5, 4, 2, 9, 10, 11, 7, 6, 12]
 
-example4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-result4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+max_example4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+min_example4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+max_result4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+min_result4 = [1, 2, 4, 3, 6, 5, 8, 10, 7, 9]
 
+max_example5 = [1, 16, 5, 30, 27, 17, 20, 2, 57, 3, 90]
+min_example5 = [1, 16, 5, 30, 27, 17, 20, 2, 57, 3, 90]
+max_result5 = [90, 57, 20, 30, 27, 17, 5, 2, 1, 3, 16]
+min_result5 = [1, 2, 5, 16, 3, 17, 20, 30, 57, 27, 90]
 
 # if it returns False, it raises Assertion Error
 # if it returns True, nothing happens
 if __name__ == "__main__":
-    assert heapSort(example1) == result1
-    assert heapSort(example2) == result2
-    assert heapSort(example3) == result3
-    assert heapSort(example4) == result4
+    assert maxHeapSort(max_example1) == max_result1
+    assert minHeapSort(min_example1) == min_result1
+    assert maxHeapSort(max_example2) == max_result2
+    assert minHeapSort(min_example2) == min_result2
+    assert maxHeapSort(max_example3) == max_result3
+    assert minHeapSort(min_example3) == min_result3
+    assert maxHeapSort(max_example4) == max_result4
+    assert minHeapSort(min_example4) == min_result4
+    assert maxHeapSort(max_example5) == max_result5
+    assert minHeapSort(min_example5) == min_result5
--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Basic-algorithm-1
 
+**Think in an abstract, analytical and systematic way**
+
 ## Table of Contents   
 + [1. Sorting Algorithms](#1-sorting-algorithms)
   + [1.1 Bubble Sort](#11-bubble-sort)
   + [1.2 Selection Sort](#12-selection-sort)
   + [1.3 Insertion Sort](#13-insertion-sort)
   + [1.4 Shell Sort](#14-shell-sort)
+  + [1.5 Merge Sort](#15-merge-sort)
+  + [1.6 Quick Sort](#16-quick-sort)
+  + [1.7 Binary Heap](#17-binary-heap)
 
 
 ## 1. Sorting Algorithms
-
 
 ### 1.1 Bubble Sort
 [https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/BubbleSort](https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/BubbleSort)   
@@ -24,4 +28,16 @@
 
 
 ### 1.4 Shell Sort   
+[https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/ShellSort](https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/ShellSort)
 
+
+### 1.5 Merge Sort
+[https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/MergeSort](https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/MergeSort)
+
+
+### 1.6 Quick Sort
+[https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/QuickSort](https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/QuickSort)
+
+
+### 1.7 Binary Heap
+[https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/BinaryHeap](https://github.com/SeiwonPark/Basic-algorithm-1/tree/main/BinaryHeap)
